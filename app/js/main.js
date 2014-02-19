ab7fc5b318831abe4804897e1ebbad76d0ffd7bc
Marek Kliś <marek.klis@gmail.com>
2014-02-19 15:34:22 +0100

    items view initialized

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-var apteczka = angular.module('apteczka', []);
+var apteczkaConfig = function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'ItemsCtrl',
+            templateUrl: 'view/items.html'
+        })
+    ;
+};
+var apteczka = angular.module('apteczka', ['ngRoute']).config(apteczkaConfig);
